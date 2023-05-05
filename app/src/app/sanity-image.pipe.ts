2318d030f52b912b2e4ab5e7122c760078da1cd6
Knut Melvær <knut@sanity.io>
2023-05-05 08:42:49 -0700

    Add Sanity image pipe

---
+++ app/src/app/sanity-image.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { SanityService } from './sanity.service';
+
+@Pipe({ name: 'sanityImage' })
+export class SanityImagePipe implements PipeTransform {
+  constructor(private sanityService: SanityService) {}
+
+  transform(value: SanityImageSource, width?: number): string {
+    if (width) {
+      return this.sanityService
+        .getImageUrlBuilder(value)
+        .width(width)
+        .auto('format')
+        .url();
+    }
+    return this.sanityService.getImageUrlBuilder(value).auto('format').url();
+  }
+}
