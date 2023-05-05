d694b64b313fc50cf10338c9b75c515cf79013a7
Knut Melvær <knut@sanity.io>
2023-05-05 08:42:09 -0700

    remove process.env from environments

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   sanity: {
-    projectId: process.env['SANITY_PROJECT_ID'] || 'anokeucs',
-    dataset: process.env['SANITY_DATASET'] || 'production',
+    projectId: 'anokeucs',
+    dataset: 'production',
     apiVersion: '2022-05-01',
     useCdn: true, // set to false for fresh data
   },
