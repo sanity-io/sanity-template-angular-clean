ed0eb8a690a140b20022489789b888058f1a3933
Knut Melvær <knut@sanity.io>
2023-05-05 08:43:30 -0700

    Fetch posts on home page

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { SanityService } from './../sanity.service';
+import { Component, OnInit } from '@angular/core';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { Observable } from 'rxjs';
+
+export interface Post {
+  _id: string;
+  title: string;
+  excerpt?: string;
+  slug: {
+    current: string;
+  };
+  mainImage?: SanityImageSource;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  posts$: Observable<Post[]>;
 
+  constructor(private sanityService: SanityService) {
+    this.posts$ = this.sanityService.fetch(
+      '*[_type == "post" && defined(slug.current)]|order(_createdAt desc)'
+    );
+  }
+  ngOnInit(): void {}
 }
