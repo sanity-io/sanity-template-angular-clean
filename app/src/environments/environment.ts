bee6eb2a977c78f7e5656d629d831a8195cfc0ce
Knut Melvær <knut@sanity.io>
2023-05-11 07:53:32 -0700

    Throw error if variables are not set

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,8 +1,17 @@
+const PROJECT_ID = '';
+const DATASET = '';
+
+if (!PROJECT_ID || !DATASET) {
+  throw new Error(
+    'Sanity project ID and dataset name are required. Go into `app/src/environments/environment.ts` and set them.'
+  );
+}
+
 export const environment = {
   production: false,
   sanity: {
-    projectId: 'anokeucs',
-    dataset: 'production',
+    projectId: PROJECT_ID,
+    dataset: DATASET,
     apiVersion: '2022-05-01',
     useCdn: true, // set to false for fresh data
   },
