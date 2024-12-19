a14b6aa7dae2dba3ef408c9ca48eebe638ded9c5
Ken Jones <ken@sanity.io>
2024-12-19 14:00:12 -0600

    Standardize Sanity template (#32)
    * Standardize template
    
    * Update Readme
    
    * Add Validate Workflow
    
    * Add script to generate environment file

---
+++ angular-app/src/app/card/card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from "@angular/core";
+import { Post } from "src/types";
+
+@Component({
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"],
+})
+export class CardComponent {
+  @Input() post: Post;
+
+  constructor() {
+    this.post = {
+      _id: "1",
+      title: "My first post",
+      excerpt: "This is my first post.",
+      slug: {
+        current: "my-first-post",
+      },
+      mainImage: {
+        asset: {
+          url: "https://via.placeholder.com/400",
+        },
+        alt: "Placeholder image",
+      },
+    };
+  }
+}
